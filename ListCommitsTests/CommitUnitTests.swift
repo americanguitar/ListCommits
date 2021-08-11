3884ecf0c2281fa54537e436690223ed5c7fb95e
Tom Limbaugh <tomlimbaugh@me.com>
2021-08-11 19:20:04 -0400

    Initial commit. Added unit tests for Commit object. Will fail until code is written.

---
+++ ListCommitsTests/CommitUnitTests.swift
@@ -0,0 +1,48 @@
+//
+//  ListCommitsTests.swift
+//  ListCommitsTests
+//
+//  Created by Tom Limbaugh on 8/11/21.
+//
+
+import XCTest
+@testable import ListCommits
+
+class CommitUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        var newCommit = Commit(author: "Tom Limbaugh", hash: "e3365be8c8992cefc303b9504bd6face0e998479", message: "initial commit")
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testThatCommitExists() throws {
+        
+        XCTAssertNotNil(newCommit, "Should be able to create an instance of Commit")
+    }
+    
+    func testThatCommitCanHaveAnAuthor() {
+        
+        XCTAssertEqual(newCommit.author, "Tom Limbaugh", "Should be able to have author")
+    }
+    
+    func testThatCommitCanHaveHashValue() {
+        
+        XCTAssertEqual(newCommit.hash, "e3365be8c8992cefc303b9504bd6face0e998479", "Should be able to have hash value")
+    }
+    
+    func testThatCommitCanHaveAMessage() {
+        
+        XCTAssertEqual(newCommit.message, "initial commit", "Should be able to have commit message")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
