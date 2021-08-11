a468661bfaeab971f9767499591605f5df8473aa
Tom Limbaugh <tomlimbaugh@me.com>
2021-08-11 19:10:34 -0400

    Initial Commit

---
+++ ListCommits/ListCommitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListCommitsApp.swift
+//  ListCommits
+//
+//  Created by Tom Limbaugh on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ListCommitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
